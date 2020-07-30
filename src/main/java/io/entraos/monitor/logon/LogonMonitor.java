c6944ec3d2734e7f0068815e847d5d45bb8a4cfa
baardl <bard.lind@gmail.com>
2020-07-30 15:25:28 +0200

    #1 Test with wiremock

---
+++ src/main/java/io/entraos/monitor/logon/LogonMonitor.java
@@ -0,0 +1,69 @@
+package io.entraos.monitor.logon;
+
+import org.slf4j.Logger;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URLEncoder;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+public class LogonMonitor {
+    private static final Logger log = getLogger(LogonMonitor.class);
+    protected HttpClient client;
+    private HttpRequest httpRequest;
+    private final URI logonUri;
+
+    public LogonMonitor(URI logonUri) {
+        this.logonUri = logonUri;
+        client = HttpClient.newBuilder().build();
+    }
+
+    /*
+    Eg:
+    "grant_type=password"
+     "username=baardl-test"
+     "password=anything"
+     */
+    public HttpResponse postLogon(Map<Object, Object> body) {
+
+        httpRequest = HttpRequest.newBuilder()
+                .header("Content-Type", "application/x-www-form-urlencoded")
+                .uri(logonUri)
+                .POST(ofFormData(body))
+                .build();
+        HttpResponse<String> response = null;
+        try {
+            response = client.send(httpRequest, HttpResponse.BodyHandlers.ofString());
+            // print status code
+            System.out.println(response.statusCode());
+
+            // print response body
+            System.out.println(response.body());
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        return response;
+    }
+
+    public static HttpRequest.BodyPublisher ofFormData(Map<Object, Object> data) {
+        var builder = new StringBuilder();
+        for (Map.Entry<Object, Object> entry : data.entrySet()) {
+            if (builder.length() > 0) {
+                builder.append("&");
+            }
+            builder.append(URLEncoder.encode(entry.getKey().toString(), StandardCharsets.UTF_8));
+            builder.append("=");
+            builder.append(URLEncoder.encode(entry.getValue().toString(), StandardCharsets.UTF_8));
+        }
+        return HttpRequest.BodyPublishers.ofString(builder.toString());
+    }
+}
---
+++ src/test/java/io/entraos/monitor/logon/LogonMonitorTest.java
@@ -0,0 +1,71 @@
+package io.entraos.monitor.logon;
+
+import com.github.tomakehurst.wiremock.WireMockServer;
+import com.github.tomakehurst.wiremock.client.WireMock;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpResponse;
+import java.util.HashMap;
+import java.util.Map;
+
+import static com.github.tomakehurst.wiremock.client.WireMock.*;
+import static com.github.tomakehurst.wiremock.core.WireMockConfiguration.wireMockConfig;
+import static no.cantara.config.ServiceConfig.getProperty;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.slf4j.LoggerFactory.getLogger;
+
+
+class LogonMonitorTest {
+    private static final Logger log = getLogger(LogonMonitorTest.class);
+    private static WireMockServer server;
+    private static int port;
+    private static URI logonUri;
+
+    @BeforeAll
+    static void setUp() {
+        server = new WireMockServer(wireMockConfig().dynamicPort()); //No-args constructor will start on port 8080, no HTTPS
+        server.start();
+        port = server.port();
+        logonUri = URI.create("http://localhost:" + port + "/logon");
+    }
+
+    @Test
+    void postLogon() {
+        Map<Object, Object> data = new HashMap<>();
+        data.put("grant_type", getProperty("logon_grant_type"));
+        data.put("username", getProperty("logon_username"));
+        data.put("password", getProperty("logon_password"));
+        WireMock.configureFor("localhost", port);
+        stubFor(post(urlEqualTo("/logon"))
+                .withHeader("Content-Type", equalTo("application/x-www-form-urlencoded"))
+                .willReturn(aResponse()
+                        .withStatus(200)
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("<response>Some content</response>")));
+        LogonMonitor logonMonitor = new LogonMonitor(logonUri);
+        HttpResponse response = logonMonitor.postLogon(data);
+        assertNotNull(response);
+    }
+
+    public static void main(String[] args) throws URISyntaxException {
+        Map<Object, Object> data = new HashMap<>();
+        data.put("grant_type", getProperty("logon_grant_type"));
+        data.put("username", getProperty("logon_useranme"));
+        data.put("password", getProperty("logon_password"));
+        String logonUriProperty = getProperty("logon_uri");
+        URI logonUri = new URI(logonUriProperty);
+        LogonMonitor logonMonitor = new LogonMonitor(logonUri);
+        HttpResponse response = logonMonitor.postLogon(data);
+        log.info("Response: {}", response);
+    }
+
+    @AfterAll
+    static void afterAll() {
+        server.stop();
+    }
+}
