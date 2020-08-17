fe4e81edae9f63c9cfd924209b736d4ca6399ad4
baardl <bard.lind@gmail.com>
2020-08-17 11:30:43 +0200

    Validate Healthy status

---
+++ src/test/java/io/entraos/monitor/health/HealthResourceTest.java
@@ -0,0 +1,36 @@
+package io.entraos.monitor.health;
+
+import io.entraos.monitor.MonitorService;
+import org.junit.Before;
+import org.junit.Test;
+
+import javax.ws.rs.core.Response;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class HealthResourceTest {
+
+    private MonitorService monitorService = null;
+    private HealthResource healthResource;
+
+    @Before
+    public void setUp() throws Exception {
+        monitorService = mock(MonitorService.class);
+        healthResource = new HealthResource(monitorService);
+    }
+
+    @Test
+    public void getMessageHealthy() {
+        when(monitorService.isStatusOk()).thenReturn(true);
+        String healthyJson = "{\"status\": \"ok\"";
+        when(monitorService.toJson()).thenReturn(healthyJson);
+        Response response = healthResource.getMessage();
+        assertNotNull(response);
+        assertEquals(200, response.getStatus());
+        String body = (String) response.getEntity();
+        assertEquals(healthyJson, body);
+    }
+}
